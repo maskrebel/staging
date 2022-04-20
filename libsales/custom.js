68ed45e10bdccb3267c33d35403870a003e31359
fadil <wardi.fadillah@detik.com>
2022-04-20 17:18:23 +0700

    first staging

---
+++ libsales/custom.js
@@ -0,0 +1,262 @@
+$(".headertop").html(`<div class="header-30">
+                    <a href="#">DAYA TOYOTA CAKUNG</a>
+                </div>
+                <div class="header-70">
+                    Jl. Raya Bekasi No.KM 26,5 Jakarta Timur\t\t</div>
+                <div style="clear: both"></div>`)
+
+$("#headerwrap").html(`<div id="header">
+\t\t\t\t\t<div class="headlogo">
+\t\t\t\t\t\t<a href="https://www.kredittoyotajabotabek.com/">
+\t\t\t\t\t\t\t<img src='https://www.kredittoyotajabotabek.com/file/20130806220256.png'  alt='Kredit Daya Toyota Cakung | Promo Daya Toyota Cakung | Harga Daya Toyota Cakung' class="imgpng">
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="headkontak">
+\t\t\t\t\t\t<a href="#kontak-kami" class="pop">
+\t\t\t\t\t\t\t<button name="kontak">
+\t\t\t\t\t\t\t\tHUBUNGI KAMI
+\t\t\t\t\t\t\t</button>
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div style="clear: both"></div>
+\t\t\t\t</div>
+\t\t\t\t<div id="tickerwrap">
+\t\t\t\t\t<div class="ticker">
+\t\t\t\t\t</div>
+\t\t\t\t\t<div style="clear: both"></div>
+\t\t\t\t</div>
+\t\t\t\t<div class="toggleMenu">MENU</div>
+\t\t\t\t<div class="modbutton">
+\t\t\t\t\t<i class="glyphicon glyphicon-search" style="font-size:70%;margin-right:5px"></i> MODEL TOYOTA
+\t\t\t\t</div>
+\t\t\t\t<div style="clear: both"></div>
+\t\t\t\t<div id="listmodel" style="display: none;">
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Agya">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Agya</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/nagya-280.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Calya">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Calya</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/ncalya-280.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="All New Avanza">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">All New Avanza</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/avanza2021-280.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Rush">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Rush</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/All-New-Rush-280x190.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="All New Veloz">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">All New Veloz</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/veloz2021-280.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Raize">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Raize</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/toyota-raize-kedaiwebsite.com-280.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Fortuner">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Fortuner</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/fortuner2022-280.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Kijang Innova">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Kijang Innova</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/toyota-new-innova-kedaiwebsite.com-280.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="C-HR Hybrid">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">C-HR Hybrid</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/chr-h-280.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Vios">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Vios</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/Vios-280x190.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Corolla Altis">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Corolla Altis</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/6-1-280x190.png" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Camry">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Camry</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/6-280x190.png" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Hilux">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Hilux</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/hilux20-280.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Hi-Ace">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Hi-Ace</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/2-280x190.png" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="All New Land Cruiser">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">All New Land Cruiser</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/landcruiser2022-280.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Alphard">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Alphard</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/nap-tb.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Vellfire">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Vellfire</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/vell-tb.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Corolla Cross">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Corolla Cross</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/ccross-280.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="New Voxy">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">New Voxy</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/newvoxy-280.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+\t\t\t\t\t<div class="listmod">
+\t\t\t\t\t\t<a href="https://www.toyota.astra.co.id/shopping-tools/e-brochure" target="_blank"  title="Sienta">
+\t\t\t\t\t\t\t<div class="arealist">
+\t\t\t\t\t\t\t\t<div class="listlabel">Sienta</div>
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t<img width="120" height="90" src="https://www.kredittoyotajabotabek.com/file/sienta-280.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="">
+\t\t\t\t\t\t</a>
+\t\t\t\t\t</div>
+
+\t\t\t\t\t<div style="clear: both"></div>
+\t\t\t\t</div>
+
+\t\t\t\t<div id="topheadmenu">
+\t\t\t\t\t<ul class="naviku">
+\t\t\t\t\t\t<li class=""><a href="index.html">Beranda</a></li>
+\t\t\t\t\t\t<li><a href="harga.html">Harga</a></li>
+\t\t\t\t\t\t<li><a href="galery.html">Gallery Delivery</a></li>
+\t\t\t\t\t\t<!--\t\t\t\t\t<li><a href="https://www.kredittoyotajabotabek.com/artikel" class="parent">Artikel</a>-->
+
+\t\t\t\t\t\t<!--\t\t\t\t\t\t<ul><li class='cat-item cat-item-1'><a href='https://www.kredittoyotajabotabek.com/category/2/berita-terbaru' >Berita Terbaru</a></li><li class='cat-item cat-item-1'><a href='https://www.kredittoyotajabotabek.com/category/3/event-terbaru' >Event Terbaru</a></li><li class='cat-item cat-item-1'><a href='https://www.kredittoyotajabotabek.com/category/1/promo-terbaru' >Promo Terbaru</a></li></ul>\t\t\t\t\t\t-->
+\t\t\t\t\t\t<!--\t\t\t\t\t</li>-->
+\t\t\t\t\t</ul>
+
+\t\t\t\t\t<div style="clear: both"></div>
+\t\t\t\t</div>`)
+
+$("#kontak-kami").html(`<div class="bigcontact">
+\t\t\t<div class="big1">
+\t\t\t\t<img src="profile.jpeg" alt="" />
+\t\t\t\t<div id="botname" class="bigtitle" style="font-size:150%;margin-top:10px;text-align:center">RIZKY</div>
+\t\t\t\t<div id="botname" style="font-size:90%;color:#BBB;margin-bottom:10px;text-align:center">Senior Sales Executive</div>
+\t\t\t</div>
+\t\t\t<div class="big2">
+\t\t\t\t<h2 id="topname" class="bigtitle" style="margin-top:0">RIZKY</h2>
+\t\t\t\t<div id="topname" style="font-size:160%;color:#BBB;margin:-10px 0 10px 0">Senior Sales Executive</div>
+\t\t\t\t<div style="clear: both">&nbsp;</div>
+\t\t\t\t<ul>
+\t\t\t\t\t<li class="ktelp"><a href="tel:082213512990" title="Klik untuk telpon langsung">0822.1351.2990</a></li>
+\t\t\t\t\t<li class="kwa"><a href="https://api.whatsapp.com/send?phone=6282213512990&text=Halo Pak Rizky, saya ingin bertanya mengenai promo mobil Toyota " target="_blank">0822.1351.2990</a></li>
+\t\t\t\t\t<li class="kemail"><a href="mailto:mailto:rizki.hardiansyah13@gmail.com" title="Klik untuk kirim email">mailto:rizki.hardiansyah13@gmail.com</a></li>
+\t\t\t\t</ul>
+\t\t\t\t<div style="clear: both">&nbsp;</div>
+\t\t\t</div>
+\t\t\t<div style="clear: both"></div>
+\t\t\t<div style="margin-top:10px;text-align:center;padding-top:10px;border-top:5px solid #EEE">
+\t\t\t\t<a href="http://rizkytoyotacakung.com/">DAYA TOYOTA</a><br>Jl. Raya Bekasi No.KM 26,5, RT.7/RW.5, Ujung Menteng, Kec. Cakung, Kota Bks, Daerah Khusus Ibukota Jakarta 13960<br>
+\t\t\t\t\t\tKunjungi website kami di  <a href="http://rizkytoyotacakung.com/">Kredit Daya Toyota Cakung</a>
+\t\t\t</div>
+\t\t</div>`)
+
+$("#footerwrap").html(`<div class="footbarwidget"></div>
+\t\t\t<div class="footbarwidget"></div>
+\t\t\t<div style="clear: both"></div>
+\t\t\t<div class="footer">
+\t\t\t\tKredit Daya Toyota Cakung | Promo Daya Toyota Cakung | Harga Daya Toyota Cakung
+\t\t\t\t<div style="clear: both"></div>
+\t\t\t\t<div style="font-size:80%;clear: both">Developed by Wardi Fadillah</div>
+\t\t\t\t<div class="spacefooter"></div>
+\t\t\t</div>`)
+
+$("#medsosfooter").html(`<a href="tel:082213512990"  title="Call" >
+\t\t\t<img src="https://www.kredittoyotajabotabek.com/images/call.jpg" border="0" style="margin-bottom:5px;" class="imgpng"></a>
+\t\t<br>
+\t\t\t<a href="https://api.whatsapp.com/send?phone=6282213512990&text=Halo Pak Rizky, saya ingin bertanya mengenai promo mobil Toyota "  target="_blank" title='Whatsapp'>
+\t\t\t\t<img src="https://www.kredittoyotajabotabek.com/images/wa.jpg" border="0" style="margin-bottom:5px;"  class="imgpng"></a>
+\t\t<br>`)
+
+$("#medsosfooter2").html(`<table cellspacing=0 cellpadding=0 border=0 width="100%">
+\t\t\t<tr>
+\t\t\t\t<td align="center"><a href="tel:082213512990"  title="Call" >
+\t\t\t\t\t<img src="https://www.kredittoyotajabotabek.com/images/call.jpg" border="0" style="margin-bottom:0px;" width="40" class="imgpng"><br><font color="white" style='font-size:11px;font-weight:bolder;'>CALL</font></a>
+\t\t\t\t</td>
+\t\t\t\t<td align="center" STYLE="padding-top:5px;">
+\t\t\t\t\t<a href="https://api.whatsapp.com/send?phone=6282213512990&text=Halo Pak Rizky, saya ingin bertanya mengenai promo mobil Toyota "  target="_blank" title='Whatsapp'>
+\t\t\t\t\t\t<img src="https://www.kredittoyotajabotabek.com/images/wa.jpg" border="0" style="margin-bottom:5px;" width="30" class="imgpng"><br><font color="white" style='font-size:11px;font-weight:bolder;'>WHATSAPP</font></a>
+\t\t\t\t</td>
+\t\t\t</tr>
+\t\t</table>`)
